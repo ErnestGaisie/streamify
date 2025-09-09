5317274d1e13de7e4de867fef3f9da7ca0d13bd4
Ernest Gaise <ernestyawgaisie@gmail.com>
2025-09-09 12:33:13 -0400

    (feat): Theme Selector

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,21 +10,22 @@ import OnboardingPage from "./pages/OnboardingPage.jsx";
 import { Toaster } from "react-hot-toast";
 import useAuthUser from "./hooks/useAuthUser.js";
 import Layout from "./components/Layout.jsx";
+import { useThemeStore } from "./store/useThemeStore.js";
 
-// import PageLoader from "./components/PageLoader.jsx";
+import PageLoader from "./components/PageLoader.jsx";
 
 
 const App = () => {
   const { isLoading, authUser } = useAuthUser();
-  // const { theme } = useThemeStore();
+  const { theme } = useThemeStore();
 
   const isAuthenticated = Boolean(authUser);
   const isOnboarded = authUser?.isOnboarded;
 
-  // if (isLoading) return <PageLoader />;
+  if (isLoading) return <PageLoader />;
 
   return (
-    <div className="h-screen" data-theme="night">
+    <div className="h-screen" data-theme={theme}>
       <Routes>
         <Route
           path="/"
